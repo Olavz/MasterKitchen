0b83b588d67bb4462ea5bea4a802f65a2f9353ae
Olav Hermansen <olavzat@gmail.com>
2014-08-30 17:00:40 +0200

    Added highlight for current week

--- list.py
+++ list.py
@@ -1,4 +1,6 @@
-#!/bin/python
+#! /usr/bin/python
+print 'Content-type: text/html\n'
+print '<style>.selected { color:#C30; font-weight:bold; }</style>'
 
 from datetime import date
 
@@ -10,16 +12,16 @@ users = [
   "Martin",
   "Per",
   "Olav",
-"Lill",
-"Julie",
-"Lise",
-"Magnus",
+  "Lill",
+  "Julie",
+  "Lise",
+  "Magnus",
 ]
 
 startWeek = 35
 ulen = len(users)
 for week in range(startWeek,52):
-
-  print("Week " + str(weekNumber)+ " - "+ users[weekNumber % ulen])
-  weekNumber+=1
-  
+  if week == weekNumber:
+    print("<span class='selected'>Week " + str(week)+ " - "+ users[week % ulen] + "</span></br>")
+  else:
+    print("<span>Week " + str(week)+ " - "+ users[week % ulen] + "</span></br>")
